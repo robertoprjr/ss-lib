592746de59451f379f5d2cc8c4045139cd06e3c3
Roberto Passos <robertoprjr@gmail.com>
2025-07-20 22:16:13 +0100

    Selection for demo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,12 +9,27 @@ object Main {
     println("--- END :: Load Session ------")
 
     println("--- BEGIN :: Main Demo ----")
-    //LoadCSVFileToDFDemo.demo(spark)
-    //JoinNullSafeDemo.demo(spark)
-    //ArrayFunctionsDemo.demo(spark)
-    //BJoinDemo.demo(spark)
-    PrepareDFDemo.demo(spark)
+    println("01. Load CSV File ")
+    println("02. Join Null Safe ")
+    println("03. Array Functions ")
+    println("04. Broadcast Join ")
+    println("05. Prepare DF ")
+    println("06. Window Spec and Substitute Option")
+    println("00. Exit ")
 
+    val inputDemoExecution = scala.io.StdIn.readLine("Enter the option (2 numbers): ")
+
+    inputDemoExecution match {
+      case "01" => LoadCSVFileToDFDemo.demo(spark)
+      case "02" => JoinNullSafeDemo.demo(spark)
+      case "03" => ArrayFunctionsDemo.demo(spark)
+      case "04" => BJoinDemo.demo(spark)
+      case "05" => PrepareDFDemo.demo(spark)
+      case _ => println("Exiting...")
+    }
+
+    println("--- END :: Execution ------")
+    println("Press any key to finish")
     Console.in.read()
     println("--- END :: Main Demo ------")
   }
---
+++ src/main/scala/demos/WindowSpecDemo.scala
@@ -0,0 +1,10 @@
+package demos
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import vars.DefaultVars
+
+object WindowSpecDemo {
+
+}
